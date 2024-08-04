df97f2e70e8df367f169f2a95ff987fce82ceeb9
milena <milena123323@gmail.com>
2024-08-04 14:50:07 -0700

    update latest msw

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -1,19 +1,24 @@
-import { rest } from 'msw';
+// import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 import { API_URL } from '@/config/constants';
 
-import { authHandlers } from './auth';
-import { customersHandlers } from './customers';
-import { exchangesHandlers } from './exchanges';
+// import { authHandlers } from './auth';
+// import { customersHandlers } from './customers';
+// import { exchangesHandlers } from './exchanges';
 
 export const handlers = [
-  ...authHandlers,
-  ...exchangesHandlers,
-  ...customersHandlers,
-  rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
+  // ...authHandlers,
+  // ...exchangesHandlers,
+  // ...customersHandlers,
+  /* rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({ healthy: true })
     );
+  }), */
+  http.get(`${API_URL}/healthcheck`, () => {
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json({ healthy: true });
   }),
 ];
